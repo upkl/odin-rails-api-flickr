4f39ff5e6bf1f71d7f463dade4602cc3b1a9677d
upkl <2737384+upkl@users.noreply.github.com>
2023-08-31 16:49:17 +0200

    connect to flickr, display photos

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,31 @@
+require 'flickr'
+
 class StaticPagesController < ApplicationController
   def home
+    return unless params[:username]
+
+    begin
+      flickr = Flickr.new(ENV.fetch('FLICKR_API_KEY', nil), ENV.fetch('FLICKR_API_SECRET', nil))
+      flickr.test.echo
+    rescue Flickr::FailedResponse
+      redirect_to '/', alert: 'Failed to connect.' and return
+    end
+
+    begin
+      @user = flickr.people.findByUsername(username: params[:username])
+    rescue Flickr::FailedResponse
+      redirect_to '/', alert: 'User not found.' and return
+    end
+
+    p @user, @user.nsid
+
+    redirect_to '/', alert: 'User not found.' and return unless @user
+
+    begin
+      @photos = flickr.people.getPublicPhotos(user_id: @user.nsid)
+      p @photos
+    rescue Flickr::FailedResponse
+      redirect_to '/', alert: 'Could not retrieve photos.'
+    end
   end
 end
